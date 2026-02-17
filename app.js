30102077da778361503142296b68a03a0d2a0bc2
tonezakarea1-hue <tonezakarea1@gmail.com>
2026-02-17 13:31:32 +0300

    Update Firebase configuration
    Updated Firebase configuration with new project details.

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
-};
+apiKey: "AIzaSyCR14F4wPiHx0to80_17tTU_ND21BHqORQ",
+  authDomain: "who-s-imposter.firebaseapp.com",
+  projectId: "who-s-imposter",
+  storageBucket: "who-s-imposter.firebasestorage.app",
+  messagingSenderId: "868731225838",
+  appId: "1:868731225838:web:20152a12dbabbee06cd5ff",
+  measurementId: "G-Z07E8HSBL5"
